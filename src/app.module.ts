0eaea15aabc3123535f846713bdb29c746c5f2f0
tangx <uyinn28@gmail.com>
2024-08-28 22:25:32 +0800

    pino logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { LoggerModule } from 'nestjs-pino'
 @Module({
-  imports: [],
+  // imports: [],
+  imports: [LoggerModule.forRoot()],
+
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
+
